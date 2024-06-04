6ce5268c79afd2824db2bd6226d6728c02aba49b
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2024-06-04 22:03:10 +0000

    wip

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Domain;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,11 +11,27 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Context
 {
-	public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
 	{
 		public ApplicationDbContext(DbContextOptions options):base(options)
 		{
 
 		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			foreach(var property in builder.Model.GetEntityTypes().SelectMany(t=>t.GetProperties()).Where(p=>p.ClrType == typeof(decimal)
+			|| p.ClrType == typeof(decimal?)))
+			{
+				property.SetColumnType("decimal(18,2)");
+			}
+			base.OnModelCreating(builder);
+
+			builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+		}
+
+
+		//public DbSet<Employee> Employees { get; set; }
+		public DbSet<Employee> Employees => Set<Employee>();
 	}
 }
---
+++ Infrastructure/DbConfig/EmployeeEntityConfig.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DbConfig
+{
+    internal class EmployeeEntityConfig : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder
+                .ToTable("Employees", SchemaNames.HR)
+                .HasIndex(e => e.FirstName)
+                .HasDatabaseName("IX_Employees_FirstName");
+
+            builder
+                .HasIndex(e => e.LastName)
+                .HasDatabaseName("IX_Employees_LastName");
+        }
+    }
+
+}
