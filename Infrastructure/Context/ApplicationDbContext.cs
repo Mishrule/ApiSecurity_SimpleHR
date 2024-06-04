cb99858c4a7466bea09c579d79edbfd9b1ecfc84
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2024-06-04 06:30:45 +0000

    Add project files.

---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+	public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+	{
+		public ApplicationDbContext(DbContextOptions options):base(options)
+		{
+
+		}
+	}
+}
